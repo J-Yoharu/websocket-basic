1a0af76a75ea01a6fece16444b7362b30376d3b1
Yoharu <jonathas_a.a@hotmail.com>
2022-05-27 07:51:45 -0300

    Exemplos diversos de io e manager (namespace / room)

--- index.js
+++ index.js
@@ -1,19 +1,89 @@
-const WebSocket = require("ws");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 const port = 3000;
 const host = "localhost";
 
-const socket = new WebSocket.Server({ port, host });
-const clients = [];
+const httpServer = createServer({ host }, (req, res) =>
+  res.end("Http server is running")
+);
+middlewares = {
+  authMiddleware(socket, next) {
+    /*autenticação
+  payload da conexão com client: {
+        auth: {
+            token:123
+        },
+    }
+  */
+    if (!socket.handshake.auth.token) return next(new Error("Token ausente!"));
+    if (socket.handshake.auth.token != token)
+      return next(
+        new Error(
+          "Credenciais invalidas, insira a senha '123' em localstorage.token"
+        )
+      );
+    return next();
+  },
+};
 
-socket.on("connection", (client) => {
-  console.log("Foi conectado novo cliente");
-  clients.push(client);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://127.0.0.1:5500"],
+  },
 });
 
-setInterval(() => {
-  const msg = `Nova mensagem ${Math.random() * 100}`;
+const usersId = [];
+const token = "123";
 
-  clients.forEach((client) => {
-    client.send(msg);
+io.on("connection", (socket) => {
+  usersId.push(socket.id);
+  console.log(`Novo client conectado ao server wss, total: ${usersId.length}`);
+  // padrão para colocar em todo socket
+
+  socket.on("error", (err) => {
+    console.log(`Erro: ${err}`);
+    socket.disconnect();
   });
-}, 1000);
+
+  socket.broadcast.emit("user-connected", socket.id);
+
+  socket.on("disconnect", () => {
+    userIndex = usersId.findIndex((id) => id == socket.id);
+    if (userIndex == -1) return;
+
+    usersId.splice(0, userIndex);
+    console.log(usersId.length);
+    socket.broadcast.emit("user-disconnected", socket.id);
+  });
+
+  //middlewares
+  socket.use(([event, ...args], next) => {
+    // console.log(`evento ${args}`);
+    if (!usersId.includes(socket.id)) return next(new Error("ERRO BOLADO"));
+    return next();
+  });
+});
+
+//middlewares
+//autenticação via token
+io.use(middlewares.authMiddleware);
+
+const namespace = "chat";
+
+io.of(`/${namespace}`).on("connection", (socket) => {
+  socket.on("new-message", (data, callback) => {
+    /* Insere callback para avisar que a mensagem foi 
+       salva ao banco de dados
+    */
+    const save = true;
+    if (!save) return callback(save);
+
+    callback(save);
+    socket.broadcast.emit("read-message", data);
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
